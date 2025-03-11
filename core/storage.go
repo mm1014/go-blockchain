c7763092991132d11d0e89008fbd2f3a3fb0b7a7
limingjie <2218720462@qq.com>
2025-03-11 14:17:12 +0800

    create the data-structure of blockchain

---
+++ core/storage.go
@@ -0,0 +1,14 @@
+package core
+
+type Storage interface {
+	Put(*Block) error
+}
+type MemoryStore struct{}
+
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{}
+}
+
+func (s *MemoryStore) Put(b *Block) error {
+	return nil
+}
